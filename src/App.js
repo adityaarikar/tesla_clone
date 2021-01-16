3835bf37e2546be03d6176b7a6ea5e633b895b44
Aditya Arikar <adityaarikar.26@gmail.com>
2021-01-16 16:59:00 +0530

    Header Section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>welcome...</h1>
-      <h2>I am Aditya Arikar</h2>
+      <Header />
     </div>
   );
 }
