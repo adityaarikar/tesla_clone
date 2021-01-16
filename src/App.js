79628827b72e89cfddb0db169172f472d2464fff
Aditya Arikar <adityaarikar.26@gmail.com>
2021-01-16 16:11:29 +0530

    Clean Up the project setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>welcome...</h1>
+      <h2>I am Aditya Arikar</h2>
+    </div>
+  );
+}
+
+export default App;
